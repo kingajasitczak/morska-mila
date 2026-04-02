97e2f0f842a02d138d428013f2a207fc44bdb65d
Kinga <kingajasitczak@gmail.com>
2026-04-02 21:45:23 +0200

    sitemap

---
+++ src/app/sitemap.ts
@@ -0,0 +1,34 @@
+import { MetadataRoute } from 'next'
+
+export default function sitemap(): MetadataRoute.Sitemap {
+    return [
+        {
+            url: 'https://morska-mila.pl',
+            lastModified: new Date(),
+        },
+        {
+            url: 'https://morska-mila.pl/o-nas',
+            lastModified: new Date(),
+        },
+        {
+            url: 'https://morska-mila.pl/oferta',
+            lastModified: new Date(),
+        },
+        {
+            url: 'https://morska-mila.pl/cennik',
+            lastModified: new Date(),
+        },
+        {
+            url: 'https://morska-mila.pl/galeria',
+            lastModified: new Date(),
+        },
+        {
+            url: 'https://morska-mila.pl/kopan',
+            lastModified: new Date(),
+        },
+        {
+            url: 'https://morska-mila.pl/kontakt',
+            lastModified: new Date(),
+        },
+    ]
+}
